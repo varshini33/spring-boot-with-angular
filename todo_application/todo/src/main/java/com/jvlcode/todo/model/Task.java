63f4941bdf7c5d9e5d4361812c85eca06fe377b4
HAMSAA VARSHINI B <106169276+varshini33@users.noreply.github.com>
2024-12-18 17:36:27 +0530

    Add files via upload

---
+++ todo_application/todo/src/main/java/com/jvlcode/todo/model/Task.java
@@ -0,0 +1,36 @@
+package com.jvlcode.todo.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Table(name = "tasks")
+@Entity
+public class Task {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isCompleted() {
+		return completed;
+	}
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	private String description;
+	private boolean completed;
+	
+}
